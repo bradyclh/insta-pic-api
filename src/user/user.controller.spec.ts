ffc0ace0f74965ebaffb55f9cc5180e4828fd486
bradyclh <gohere1213@gmail.com>
2021-11-24 09:02:22 +0800

    add unit test for user

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,104 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            find: jest.fn().mockResolvedValue([
+              {
+                "id": 1,
+                "username": "admin",
+                "status": "REGISTERED",
+                "createdAt": "2021-11-23T16:47:09.068Z",
+                "updatedAt": "2021-11-23T16:47:09.068Z",
+                "deletedAt": null,
+                "roles": [
+                  {
+                    "id": 2,
+                    "role": "ADMIN",
+                    "createdAt": "2021-11-23T16:47:46.188Z",
+                    "updatedAt": "2021-11-23T16:47:46.188Z",
+                    "deletedAt": null
+                  }
+                ]
+              },
+              {
+                "id": 2,
+                "username": "user",
+                "status": "INACTIVATED",
+                "createdAt": "2021-11-23T16:52:05.727Z",
+                "updatedAt": "2021-11-23T16:52:05.727Z",
+                "deletedAt": null,
+                "roles": [
+                  {
+                    "id": 1,
+                    "role": "USER",
+                    "createdAt": "2021-11-23T16:47:40.554Z",
+                    "updatedAt": "2021-11-23T16:47:40.554Z",
+                    "deletedAt": null
+                  }
+                ]
+              }
+            ])
+          }
+        }
+      ]
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should get user list', async () => {
+    await expect(userController.find({
+      limit: 10,
+      pageIndex: 1,
+      orderColumn: 'id',
+      orderBy: 'asc',
+    })).resolves.toEqual([
+      {
+        "id": 1,
+        "username": "admin",
+        "status": "REGISTERED",
+        "createdAt": "2021-11-23T16:47:09.068Z",
+        "updatedAt": "2021-11-23T16:47:09.068Z",
+        "deletedAt": null,
+        "roles": [
+          {
+            "id": 2,
+            "role": "ADMIN",
+            "createdAt": "2021-11-23T16:47:46.188Z",
+            "updatedAt": "2021-11-23T16:47:46.188Z",
+            "deletedAt": null
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "username": "user",
+        "status": "INACTIVATED",
+        "createdAt": "2021-11-23T16:52:05.727Z",
+        "updatedAt": "2021-11-23T16:52:05.727Z",
+        "deletedAt": null,
+        "roles": [
+          {
+            "id": 1,
+            "role": "USER",
+            "createdAt": "2021-11-23T16:47:40.554Z",
+            "updatedAt": "2021-11-23T16:47:40.554Z",
+            "deletedAt": null
+          }
+        ]
+      }
+    ]);
   });
 });
